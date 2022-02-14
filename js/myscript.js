2b99a22f3f30a241c0d7ed69a30eaa4347efa145
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-14 15:58:14 +0100

    parola invertita

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,33 @@
 // Palidroma
-// Chiedere all'utente di mettere una parola palindroma.
-// Creare una funzione che distingua la parola palindroma
+// Invertire la parola.
+
+// Chiedere all'utente di mettere una parola palindroma
+let parola = prompt('Inserisce una parola palindroma').trim();
+console.log(parola)
+
+// Separa la parola.
+let arry =  [...parola];
+console.log(arry)
+
+// Inverte la parola
+let reversed = arry.reverse();
+console.log(reversed)
+
+// Serve per fare un calcolo con le parole inverte
+let parolaUnita = ``;
+console.log(parolaUnita)
+
+// Unisce le parole Invertite
+for (let i = 0; i < reversed.length; i++) {
+
+  parolaUnita = parolaUnita + reversed[i];
+  console.log(parolaUnita)
+
+}
+
+// parola invertita
+let parolaReversed = parolaUnita;
+console.log(parolaReversed)
+
+// Creare una funzione che distingua la parola se è palindroma
+
